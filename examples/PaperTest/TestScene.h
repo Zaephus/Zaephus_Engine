1529ec4f6591663da635ff527ab593b1288771e5
Marc Rozendaal <rozendaalmarc@gmail.com>
2025-08-06 15:30:22 +0200

    Added test scene for Paper tests

---
+++ examples/PaperTest/TestScene.h
@@ -0,0 +1,97 @@
+
+#pragma once
+
+#include <vector>
+#include <thread>
+
+#include <ZEngine.h>
+#include <ZMath.h>
+
+#ifdef ENABLE_PROFILING
+#include <tracy/Tracy.hpp>
+#endif
+
+class TestScene final : public Scene {
+    DirectionalLight* light = nullptr;
+
+    Camera* cam = nullptr;
+
+    Shader* cubeShader = nullptr;
+
+    GameObject* multiCube = nullptr;
+    MultiMeshRenderer* multiRenderer = nullptr;
+
+    unsigned int cubeAmount = 10'000;
+    float size = 25.0f;
+
+    unsigned int numThreads = 1;
+
+    public:
+        void start() override {
+
+            numThreads = std::thread::hardware_concurrency() - 2;
+
+            Bounds::shouldRender = false;
+            shouldRenderAxis = false;
+
+            light = new DirectionalLight();
+            light->name = "main_light";
+            light->transform->rotate(-55.0f, 30.0f, 0.0f);
+
+            cam = Camera::createPerspectiveCamera(45.0f * ZMath::deg2rad, 0.1f, 1000.0f);
+            cam->name = "camera";
+            cam->transform->position = { 35.0f, 35.0f, 75.0f };
+            cam->transform->rotate(-20.0f, 20.0f, 0.0f);
+            // cam->addComponent(new DebugCameraController());
+
+            renderer->setClearColor(0.2f, 0.25f, 0.4f, 1.0f);
+
+            cubeShader = Shader::instancedDiffuseShader(
+                { 0.0f, 0.6f, 0.0f, 1.0f },
+                4.0f
+            );
+
+            const std::vector<Model*> models = ModelLoader::load(ModelLoader::cube, true);
+            Mesh* cubeMesh = models[0]->mesh;
+
+            multiCube = new GameObject();
+            multiRenderer = new MultiMeshRenderer(cubeMesh, cubeShader, cubeAmount);
+            multiCube->addComponent(multiRenderer);
+            multiCube->name = "Multi-cube";
+
+            for(int i = 0; i < cubeAmount; i++) {
+                multiRenderer->setInstancePosition(i, {
+                     Random::range(-size, size),
+                     Random::range(-size, size),
+                     Random::range(-size, size)
+                });
+            }
+        }
+
+        void update() override {
+#ifdef ENABLE_PROFILING
+            ZoneScopedN("scene update");
+#endif
+
+            std::vector<std::thread> threads;
+
+            const unsigned int chunkSize = cubeAmount / numThreads;
+
+            for(size_t i = 0; i < numThreads; i++) {
+                unsigned int start = i * chunkSize;
+                unsigned int length = chunkSize;
+
+                threads.emplace_back(&TestScene::rotateCubes, this, start, length);
+            }
+
+            for(std::thread& thread : threads) {
+                thread.join();
+            }
+        }
+
+        void rotateCubes(const int _start, const int _length) const {
+            for(size_t i = _start; i < _start + _length; i++) {
+                multiRenderer->rotateInstance(i, multiRenderer->getInstancePosition(i).normalized() * 25.0f * Time::deltaTime);
+            }
+        }
+};
